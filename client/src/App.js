fdeb15eb3b09fcc40d5a0c9b7be3ef90b124627a
thesergioliveira <thesergioliveira@gmail.com>
2021-10-14 16:56:53 +0200

    addin login and register in the navigation routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
-<<<<<<< HEAD
-=======
 import About from './components/footer/About';
 import Services from './components/Services';
->>>>>>> e72ed3f (adding register component to the application)
 import Shop from './components/Shop/Shop';
-import Services from './components/Services';
 import Events from './components/Events';
 import Rent from './components/Rent';
 import BalletAndPilates from './components/BalletAndPilates';
 import TechnicalConsulting from './components/TechnicalConsulting';
 import Footer from './components/Footer';
-<<<<<<< HEAD
-import About from './components/About';
-import Contact from './components/Contact';
-=======
 import Contact from './components/footer/Contact';
 import Login from './components/Login';
 import Register from './components/Register';
->>>>>>> e72ed3f (adding register component to the application)
 
 const App = () => {
   return (
@@ -30,7 +21,6 @@ const App = () => {
         <Switch>
           <Route path="/" exact render={Home}></Route>
           <Route path="/shop" exact render={Shop}></Route>
-<<<<<<< HEAD
           <Route path="/services" exact render={Services}></Route>
           <Route path="/events" exact render={Events}></Route>
           <Route path="/rent" exact render={Rent}></Route>
@@ -38,11 +28,8 @@ const App = () => {
           <Route path="/technic" exact render={TechnicalConsulting}></Route>
           <Route path="/about" exact render={About}></Route>
           <Route path="/contact" exact render={Contact}></Route>
-=======
-          <Route path="/contact" exact render={Contact}></Route>
           <Route path="/login" exact render={Login}></Route>
           <Route path="/register" exact render={Register}></Route>
->>>>>>> e72ed3f (adding register component to the application)
         </Switch>
       </main>
       <Footer />
