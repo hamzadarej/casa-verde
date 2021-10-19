277aa252d1cb683b9c3a1f851c9691fdcd21467f
angelo.p <mrpalaiologos@gmail.com>
2021-10-19 09:54:14 +0200

    development

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,8 +7,8 @@ var router = express.Router();
 //user route
 
 /* GET users listing. */
-router.post("/register",middleware.validator,allControllers.addUser);
-router.post("/login",allControllers.login);
-router.get("/logout",allControllers.logout);
+router.post("/register", middleware.validator, allControllers.addUser);
+router.post("/login", allControllers.login);
+router.get("/logout", allControllers.logout);
 
 module.exports = router;
--- server/routes/users.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-const allControllers = require("../controllers/controller");
-var router = express.Router();
-
-/* GET users listing. */
-router.post("/", allControllers.addUser);
-module.exports = router;
